110b94dac7ebeb4e5e63d54ed56d8377733f982f
Tim Wojtulewicz <tim@corelight.com>
2024-07-19 14:52:13 -0700

    Add pre-commit hooks, reformat everything using pre-commit

--- c-ares/portfile.cmake
+++ c-ares/portfile.cmake
@@ -1,4 +1,5 @@
-string(REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)" _c_ares_version "${VERSION}")
+string(REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)" _c_ares_version
+             "${VERSION}")
 set(_c_ares_version_major "${CMAKE_MATCH_1}")
 set(_c_ares_version_minor "${CMAKE_MATCH_2}")
 set(_c_ares_version_patch "${CMAKE_MATCH_3}")
@@ -9,14 +10,14 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DCARES_STATIC=${BUILD_STATIC}
-        -DCARES_SHARED=${BUILD_SHARED}
-        -DCARES_BUILD_TOOLS=OFF
-        -DCARES_BUILD_TESTS=OFF
-        -DCARES_BUILD_CONTAINER_TESTS=OFF
-)
+  SOURCE_PATH
+  "${SOURCE_PATH}"
+  OPTIONS
+  -DCARES_STATIC=${BUILD_STATIC}
+  -DCARES_SHARED=${BUILD_SHARED}
+  -DCARES_BUILD_TOOLS=OFF
+  -DCARES_BUILD_TESTS=OFF
+  -DCARES_BUILD_CONTAINER_TESTS=OFF)
 
 vcpkg_cmake_install()
 
@@ -26,16 +27,20 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/c-ares)
 vcpkg_fixup_pkgconfig()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string(
-        "${CURRENT_PACKAGES_DIR}/include/ares.h"
-        "#  ifdef CARES_STATICLIB" "#if 1"
-    )
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/ares.h"
+                       "#  ifdef CARES_STATICLIB" "#if 1")
 endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" OR NOT VCPKG_TARGET_IS_WINDOWS)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin") # Empty folders
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin"
+       "${CURRENT_PACKAGES_DIR}/debug/bin") # Empty folders
 endif()
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+     "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE.md"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
--- prometheus-cpp/portfile.cmake
+++ prometheus-cpp/portfile.cmake
@@ -3,15 +3,15 @@ set(SOURCE_PATH "${VCPKG_ROOT_DIR}/../prometheus-cpp")
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DUSE_THIRDPARTY_LIBRARIES=OFF # use vcpkg packages
-        -DENABLE_PULL=ON
-        -DENABLE_PUSH=OFF
-        -DGENERATE_PKGCONFIG=OFF
-        -DENABLE_COMPRESSION=ON
-        -DENABLE_TESTING=OFF
-)
+  SOURCE_PATH
+  "${SOURCE_PATH}"
+  OPTIONS
+  -DUSE_THIRDPARTY_LIBRARIES=OFF # use vcpkg packages
+  -DENABLE_PULL=ON
+  -DENABLE_PUSH=OFF
+  -DGENERATE_PKGCONFIG=OFF
+  -DENABLE_COMPRESSION=ON
+  -DENABLE_TESTING=OFF)
 
 vcpkg_cmake_install()
 
@@ -22,4 +22,5 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/prometheus-cpp")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
-configure_file("${SOURCE_PATH}/LICENSE" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
+configure_file("${SOURCE_PATH}/LICENSE"
+               "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
